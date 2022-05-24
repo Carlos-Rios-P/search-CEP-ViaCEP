0caec4427793ec2299764b65a8ac82a3c607717e
Carlos Rios <carlosriospontes@gmail.com>
2022-05-23 21:07:37 -0300

    Adding search/local route controller

---
+++ app/Http/Controllers/ViaCEPController.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Http\Controllers;
+
+
+class ViaCEPController extends Controller
+{
+    //
+    public function consultarCEP($ceps)
+    {
+        try {
+            $ceps = explode(',', $ceps);
+
+            foreach($ceps as $cep){
+
+                $url = "https://viacep.com.br/ws/{$cep}/json/";
+
+                $adress[] = json_decode(file_get_contents($url), true);
+            }
+
+            return response()->json(['sucess' => $adress], 200);
+
+        } catch(\Throwable $th){
+            return response()->json(['erro' => 'Informe um CEP válido'], 400);
+        }
+    }
+}
